f3c49d0d23e43d43701cb36b5dc989dd73b49ee7
Abhijeet Muneshwar <openingknots@gmail.com>
2023-07-16 08:51:15 +0530

    ✨ First Commit

---
+++ src/com.algorithm/string/KMP.java
@@ -0,0 +1,13 @@
+package com.algorithm.string;
+
+/**
+ * Created using IntelliJ IDEA.
+ * Author:  abhijeet,
+ * Date:    16/07/23,
+ * Time:    8:50 am
+ */
+public class KMP {
+  public static void main(String[] args) {
+    KMP kmp = new KMP();
+  }
+}
