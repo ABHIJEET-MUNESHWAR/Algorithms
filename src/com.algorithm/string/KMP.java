e237f4f740ff6815f48d21ed81006e21b025aa42
Abhijeet Muneshwar <openingknots@gmail.com>
2023-07-18 09:32:07 +0530

    🎨 Pattern matching using KMP algorithm

--- src/com.algorithm/string/KMP.java
+++ src/com.algorithm/string/KMP.java
@@ -9,5 +9,52 @@ package com.algorithm.string;
 public class KMP {
   public static void main(String[] args) {
     KMP kmp = new KMP();
+    String str = "abcxabcdabcdabcy";
+    String pattern = "abcdabcy";
+    System.out.println(kmp.subStringSearchKmp(str, pattern));
+  }
+
+  private boolean subStringSearchKmp(String str, String pattern) {
+    char[] strArray = str.toCharArray();
+    char[] patternArray = pattern.toCharArray();
+    int[] lps = computeLpsArray(patternArray);
+    int i = 0, j = 0;
+    while (i < str.length() && j < pattern.length()) {
+      if (strArray[i] == patternArray[j]) {
+        i++;
+        j++;
+      } else {
+        if (j != 0) {
+          j = lps[j - 1];
+        } else {
+          i++;
+        }
+      }
+    }
+    if (j == patternArray.length) {
+      return true;
+    }
+    return false;
+  }
+
+  private int[] computeLpsArray(char[] patternArray) {
+    int length = patternArray.length;
+    int[] lps = new int[length];
+    int index = 0;
+    for (int i = 1; i < length; ) {
+      if (patternArray[i] == patternArray[index]) {
+        lps[i] = index + 1;
+        index++;
+        i++;
+      } else {
+        if (index != 0) {
+          index = lps[index - 1];
+        } else {
+          lps[i] = 0;
+          i++;
+        }
+      }
+    }
+    return lps;
   }
 }
